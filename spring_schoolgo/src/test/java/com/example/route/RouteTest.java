a2774ed6ef4ab9478f398511802ca670272f2424
1-718-8 <1-718-8@27-717-8-PC>
2016-10-26 14:00:55 +0900

    dto/mapper test해야합

---
+++ spring_schoolgo/src/test/java/com/example/route/RouteTest.java
@@ -0,0 +1,49 @@
+package com.example.route;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.config.ApplicationConfig;
+import com.example.dto.Route;
+import com.example.dto.Station;
+import com.example.dto.User;
+import com.example.repo.RouteRepo;
+import com.example.service.StationService;
+ 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes={ApplicationConfig.class})
+public class RouteTest {
+	
+	private static Logger logger = LoggerFactory.getLogger(RouteTest.class);
+	
+	@Autowired
+	SqlSessionTemplate template;
+	
+	@Autowired
+	RouteRepo rRepo;
+
+	@Test
+	public void insertRouteTest() {
+		Route route = new Route("asdf",3,"루트이름","232.3","323","출발지주소","232.2","434.3","도착지주소","200km","30",null);
+		int test = rRepo.insertRoute(route);
+	
+		logger.trace("검색결과 : {}",test );
+		assertThat(test, is(notNullValue()));
+		
+		
+	}
+
+
+}
